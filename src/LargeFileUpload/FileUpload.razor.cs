5291d22f5184cf71c21bc7586ddaa113709a8fc8
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2021-09-18 23:08:31 +0200

    Started to implement the file upload component.

---
+++ src/LargeFileUpload/FileUpload.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace LargeFileUpload {
+
+    /// <summary>
+    /// The code behind file of <see cref="FileUpload"/> component.
+    /// </summary>
+    public partial class FileUpload {
+
+#if NET6_0_OR_GREATER
+        [EditorRequired]
+#endif
+        [Parameter]
+        public string Name { get; set; } = string.Empty;
+
+        [Parameter]
+        public bool Multiple { get; set; }
+
+        [Parameter]
+        public bool Disabled {  get; set; }
+    }
+}
